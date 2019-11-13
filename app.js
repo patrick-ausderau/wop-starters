56d823136dc469f8edce8588b0df1bf70f622519
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2019-11-13 10:44:59 +0200

    upd

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Home');
+  res.render('home');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
