43797ac778e2df94b7f03b2756ba216f4acff5d7
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2019-11-27 00:59:39 +0200

    week5

---
+++ week2_public_html/js/mapbox.js
@@ -0,0 +1,13 @@
+'use strict';
+mapboxgl.accessToken = 'ask from teacher or get your own';
+const map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+  center: [60.223701,24.7586469], // starting position [lng, lat]
+  zoom: 13, // starting zoom
+});
+
+const addMarker = (coords) => {
+  map.setCenter(coords);
+  const marker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+};
