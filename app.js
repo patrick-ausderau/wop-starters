1a920342f7cfa1650105c3071730455c086f6ead
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2020-03-18 10:20:20 +0200

    fix

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Home');
+app.get('/cat', (req, res) => {
+  res.send('From this endpoint you can get cats.')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
